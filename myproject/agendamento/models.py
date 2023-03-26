d223bf060169456dcb65fd6f8a39e33ff9a7060e
Antonio Lindo <antoniolvitall@gmail.com>
2023-03-26 10:36:33 -0300

    criando app e criando models

---
+++ myproject/agendamento/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from .models import Profissional
+
+
+class Profissional(models.Model):
+    nome = models.CharField(max_length=200)
+    nome_social = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Consulta(models.Model):
+    data = models.DateField()
+    profissional = models.ForeignKey(Profissional, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Consulta com {self.profissional.nome} em {self.data}'
